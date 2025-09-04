c87417154946e10ec13f1f1888942fdeeade5b3c
nagcoop <pangadosgames@gmail.com>
2025-09-03 22:03:32 -0300

    Create gh-downloads.js

---
+++ js/gh-downloads.js
@@ -0,0 +1,10 @@
+<script src="/js/gh-downloads.js"></script>
+<script>
+  renderGithubDownloads({
+    owner: 'nagcoop',
+    repo: 'nagcoop.github.io',
+    path: 'assets/downloads',
+    branch: 'main',
+    targetId: 'downloads-list'
+  });
+</script>
